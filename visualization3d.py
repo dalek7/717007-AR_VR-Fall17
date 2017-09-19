85c0a3e4b8e7d5b4ac301c0f93bcbf6e02f761f9
seung <ncraig@naver.com>
2017-09-19 10:52:22 +0900

    initial commit

---
+++ visualization3d.py
@@ -0,0 +1,45 @@
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+
+import numpy as np
+
+
+dat = np.loadtxt ("out_20170919_010635_555719_pose.txt")
+#dat = np.loadtxt ("out_20170919_010615_996641_pose.txt")
+
+
+print dat.shape
+
+x = dat[:,12+1]
+y = dat[:,13+1]
+z = dat[:,14+1]
+print x.shape
+x2 = []
+y2 = []
+z2 = []
+
+
+for i in range(dat.shape[0]):
+    if i % 5 ==0:
+        x2.append(x[i])
+        y2.append(y[i])
+        z2.append(z[i])
+
+x2 = np.array(x2)
+y2 = np.array(y2)
+z2 = np.array(z2)
+
+print x2.shape
+
+
+fig = plt.figure()
+#ax = fig.add_subplot(111, projection='3d')
+ax = fig.gca(projection='3d')
+
+ax.scatter(x2, z2,-y2, c='r', marker='.')
+ax.set_xlabel('X')
+ax.set_ylabel('Z')
+ax.set_zlabel('Y')
+ax.set_aspect('equal')
+plt.show()
