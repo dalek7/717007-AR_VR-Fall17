cd44001c31f4eadae1290c7e89c2a163611bd418
seung <ncraig@naver.com>
2017-09-21 01:51:26 +0900

    cleaned up

--- 02-visualization_with_rotation.py
+++ 02-visualization_with_rotation.py
@@ -4,20 +4,6 @@ from mpl_toolkits.mplot3d import Axes3D
 
 import numpy as np
 import DDUtil
-from matplotlib.patches import FancyArrowPatch
-from mpl_toolkits.mplot3d import proj3d
-from matplotlib import collections  as mc
-
-class Arrow3D(FancyArrowPatch):
-    def __init__(self, xs, ys, zs, *args, **kwargs):
-        FancyArrowPatch.__init__(self, (0,0), (0,0), *args, **kwargs)
-        self._verts3d = xs, ys, zs
-
-    def draw(self, renderer):
-        xs3d, ys3d, zs3d = self._verts3d
-        xs, ys, zs = proj3d.proj_transform(xs3d, ys3d, zs3d, renderer.M)
-        self.set_positions((xs[0],ys[0]),(xs[1],ys[1]))
-        FancyArrowPatch.draw(self, renderer)
 
 dat = np.loadtxt ("pose/out_20170919_010635_555719_pose.txt")
 #dat = np.loadtxt ("pose/out_20170919_010615_996641_pose.txt")
@@ -35,7 +21,7 @@ rx = dat[:, 1:4]
 ry = dat[:, 5:8]
 rz = dat[:, 9:12]
 #print(rx[0], ry[0], rz[0])
-#print(dat[0])
+print(dat[0])
 
 x = dat[:,12+1]
 y = dat[:,13+1]
@@ -50,6 +36,7 @@ ax = fig.gca(projection='3d')
 
 ax.scatter(x, y, z, c='r', marker='.')
 
+#시작점을 별(*)로 표시
 ax.scatter(x[0], y[0], z[0], c='r', marker='*',  s=100)
 # Column vectors
 g = 0.02
