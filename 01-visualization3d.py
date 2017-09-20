78d375a4d64f5f1b6af1f33faad73d7ca978d0a8
seung <ncraig@naver.com>
2017-09-20 21:55:36 +0900

    code fixes

--- 01-visualization3d.py
+++ 01-visualization3d.py
@@ -8,34 +8,25 @@ import DDUtil
 dat = np.loadtxt ("pose/out_20170919_010635_555719_pose.txt")
 #dat = np.loadtxt ("pose/out_20170919_010615_996641_pose.txt")
 
-print dat.shape
+print('dat.shape (before) =', dat.shape)
+
+# 너무 촘촘해서 띄엄띄엄 그리기 위함
+nJump = 10
+dat = DDUtil.GetSparseIDX(dat, nJump)
+
+dat = np.array(dat)
+print('dat.shape (after) =', dat.shape)
 
 x = dat[:,12+1]
 y = dat[:,13+1]
 z = dat[:,14+1]
-
-x2 = []
-y2 = []
-z2 = []
-
-# 너무 촘촘해서 띄엄띄엄 그리기 위함
-nJump = 10
-x2 = DDUtil.GetSparseIDX(x, nJump)
-y2 = DDUtil.GetSparseIDX(y, nJump)
-z2 = DDUtil.GetSparseIDX(z, nJump)
-
-x2 = np.array(x2)
-y2 = np.array(y2)
-z2 = np.array(z2)
-
-print x2.shape
-
+print('x.shape =', x.shape)
 
 # 그림 그리기 시작
 fig = plt.figure()
 ax = fig.gca(projection='3d')
 
-ax.scatter(x2, z2,-y2, c='r', marker='.')
+ax.scatter(x, z,-y, c='r', marker='.')
 ax.set_xlabel('X')
 ax.set_ylabel('Z')
 ax.set_zlabel('Y')
